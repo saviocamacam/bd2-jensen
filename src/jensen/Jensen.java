61c45400bc7f17dfcabbab5822bf2ba29ef5c93d
saviocamacam <saviocamacam@alunos.utfpr.edu.br>
2016-04-27 21:34:19 -0300

    Mudança da variável op

--- src/jensen/Jensen.java
+++ src/jensen/Jensen.java
@@ -6,7 +6,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Jensen {
-    public static int op;
+    public static int opcao;
     static int nro_itens;
     static int nro_transacoes;
     static int nro_acessos;
@@ -14,8 +14,8 @@ public class Jensen {
     public static void main(String[] args) throws IOException {
     	do {
     		System.out.println("(1) Gerar transacoes: \n(2) Gerar Schedule:\n(3) Escalonar Schedule:\n");
-    		op = new Scanner(System.in).nextInt();
-    		if(op == 1) {
+    		opcao = new Scanner(System.in).nextInt();
+    		if(opcao == 1) {
     			System.out.println("Quantos itens de dados devem ser criados? ");
     	    	nro_itens = new Scanner(System.in).nextInt();
     	    	while (nro_itens > 26) {
@@ -33,7 +33,7 @@ public class Jensen {
     	        /* Os parâmetros do usuário são enviados para a classe TransacaoManager que gerencia a criação de transações retornando uma listas delas.
     	         * Depois disso há a gravação com método implementado pela classe ArquivoManager. */
     	    	
-    		} else if (op == 2) {
+    		} else if (opcao == 2) {
     			LinkedList<Transacao> list = ArquivoManager.lerArquivoTransacao();
     	        
     			Schedule s = new Schedule(list, nro_itens, nro_transacoes, nro_acessos);
@@ -42,12 +42,12 @@ public class Jensen {
     			/* Método da classe ArquivoManager ler o arquivo retornando uma lista linkada de Transações, onde cada transação é uma lista de operações
     			 * Com isso é criado um schedule como um conjunto aleatorio de operações extraídas das operações segundo sua ordem
     			 * Depois de gerado o schedule sorteado é feita a gravação com método da classe de gerenciamento de arquivos */
-    		} else if (op == 3) {
+    		} else if (opcao == 3) {
     			
     			
     		}
     		
-    	} while (op != 4);
+    	} while (opcao != 4);
         
     }
 }
